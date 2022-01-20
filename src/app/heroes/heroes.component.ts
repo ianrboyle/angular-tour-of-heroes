215ebf190c000a8f16f5f119ee65254ccbf8ee27
Ian Boyle <irboyle21@gmail.com>
2022-01-20 11:56:43 -0500

    adds router to navigate different components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,23 +9,16 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
+  constructor(private heroService: HeroService) { }
 
- 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
-  
   ngOnInit(): void {
-    this.getHeroes()
+    this.getHeroes();
   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
