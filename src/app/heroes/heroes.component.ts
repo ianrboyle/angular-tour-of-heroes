f973c12126309b1dfc3790374a5f423bceb15e4e
Ian Boyle <irboyle21@gmail.com>
2022-01-20 11:21:06 -0500

    inject message services into heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero"
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -14,13 +15,14 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
  
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
   
   ngOnInit(): void {
     this.getHeroes()
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   getHeroes(): void {
     this.heroService.getHeroes()
